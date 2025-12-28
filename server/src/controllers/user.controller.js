01da2acc957157f60c478c357e4d160e14734ac6
MuhammadNabeelJaved <178932731+MuhammadNabeelJaved@users.noreply.github.com>
2025-12-28 23:16:13 +0300

    Add user registration controller and route

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,24 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
+import AppError from "../utils/AppError.js";
+import { successResponse } from "../utils/apiResponse.js";
+import User from "../models/User.model.js";
+
+// @desc    Register a new user
+export const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!email || !password) {
+        throw new AppError("Email and password are required", 400);
+    }
+
+    const userExists = await User.findOne(
+        [{ email }, { password }]
+    );
+    if (userExists) {
+        throw new AppError("User already exists", 409);
+    }
+
+    const user = await User.create({ name, email, password });
+
+    successResponse(res, "User registered successfully", user, 201);
+});
