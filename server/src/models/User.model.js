3e1b3a36d881d0c03aeb5ceadfc86eabdeda9f0b
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2025-12-29 00:14:41 +0300

    Implement user registration and login functionality with validation and error handling

--- server/src/models/User.model.js
+++ server/src/models/User.model.js
@@ -107,11 +107,13 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")){
+        // return next();
+    };
 
     this.password = await bcrypt.hash(this.password, 10);
-    next();
+    // next();
 });
 
 
