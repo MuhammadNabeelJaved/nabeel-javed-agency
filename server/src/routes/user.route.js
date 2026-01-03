0dd1f6921a12023cd4876b62cf8070bff891b6e4
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2026-01-03 22:49:04 +0300

    Add user authentication middleware to user routes for enhanced security

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import {
     registerUser, loginUser, getUserProfile, deleteUser, getAllUserProfile,
-    updateUserProfile, updateUserPassword, forgotPassword, resetPassword,verifyUserEmail
+    updateUserProfile, updateUserPassword, forgotPassword, resetPassword, verifyUserEmail
 } from '../controllers/user.controller.js';
 import { userAuthenticated, authorizeRoles } from '../middlewares/Auth.js';
 
@@ -10,13 +10,13 @@ const router = Router();
 router.route('/register').post(registerUser);
 router.route('/verify').post(verifyUserEmail);
 router.route('/login').post(loginUser);
-router.route('/profile/:id').get(getUserProfile);
-router.route('/:id').delete(deleteUser);
-router.route('/').get(getAllUserProfile);
-router.route('/update/:id').put(updateUserProfile);
-router.route('/update-password/:id').put(updateUserPassword);
-router.route('/forgot-password').post(forgotPassword);
-router.route('/reset-password/:token').post(resetPassword);
+router.route('/profile/:id').get(userAuthenticated, getUserProfile);
+router.route('/:id').delete(userAuthenticated, deleteUser);
+router.route('/').get(userAuthenticated, authorizeRoles('admin'), getAllUserProfile);
+router.route('/update/:id').put(userAuthenticated, updateUserProfile);
+router.route('/update-password/:id').put(userAuthenticated, updateUserPassword);
+router.route('/forgot-password').post(userAuthenticated, forgotPassword);
+router.route('/reset-password/:token').post(userAuthenticated, resetPassword);
 
 
 export default router;
