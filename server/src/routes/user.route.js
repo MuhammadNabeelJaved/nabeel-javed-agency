14fdb155f77dac5bc4d0b7f4be562403dcc3b74c
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2026-01-03 21:52:33 +0300

    Update email verification logic to use email instead of user ID

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import {
     registerUser, loginUser, getUserProfile, deleteUser, getAllUserProfile,
-    updateUserProfile, updateUserPassword, forgotPassword, resetPassword
+    updateUserProfile, updateUserPassword, forgotPassword, resetPassword,verifyUserEmail
 } from '../controllers/user.controller.js';
 import { userAuthenticated, authorizeRoles } from '../middlewares/Auth.js';
 
 const router = Router();
 
 router.route('/register').post(registerUser);
+router.route('/verify/:token').get(verifyUserEmail);
 router.route('/login').post(loginUser);
 router.route('/profile/:id').get(getUserProfile);
 router.route('/:id').delete(deleteUser);
