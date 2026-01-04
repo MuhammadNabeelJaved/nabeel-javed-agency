5f4b1119ca9043b803f3f394ab77f45fcc0b2f43
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2026-01-04 21:14:59 +0300

    Add Cloudinary integration for image upload and deletion; update multer configuration

---
+++ server/src/middlewares/Cloudinary.js
@@ -0,0 +1,49 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    //   secure_distribution: 'mydomain.com',
+    //   upload_prefix: 'https://api-eu.cloudinary.com'
+});
+
+const uploadImage = async (image) => {
+    try {
+        if (!image) {
+            throw new ApiError(400, "Image is required")
+        }
+        const result = await cloudinary.v2.uploader.upload(image, {
+            folder: "avatars"
+        })
+        fs.unlinkSync(image)
+        return result
+    } catch (error) {
+        fs.unlinkSync(image)
+        throw new ApiError(500, "Failed to upload image to Cloudinary")
+    }
+}
+
+const deleteImage = async (imageUrl) => {
+    console.log("Image URL", imageUrl)
+    try {
+        if (!imageUrl) {
+            throw new ApiError(400, "Image URL is required")
+        }
+        const publicId = "avatars/" + imageUrl.split("/").pop().split(".")[0]
+        //avatars/ko4rumwqpcgvl4r8bri5
+        console.log("publicId", publicId)
+        const result = await cloudinary.v2.uploader.destroy(publicId)
+        if (!result) {
+            throw new ApiError(500, "Failed to delete image from Cloudinary")
+        }
+        if (result.result === "ok") {
+            return true
+        }
+        return false
+    } catch (error) {
+        throw new ApiError(500, "Failed to delete image from Cloudinary")
+    }
+}
+
+export default { uploadImage, deleteImage }
