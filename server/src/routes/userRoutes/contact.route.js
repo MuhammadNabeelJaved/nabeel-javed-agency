07b32b4f97972fe47658335c95a89b70bbc9e202
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2026-01-06 23:38:35 +0300

    Add authentication and authorization middleware to contact routes

--- server/src/routes/userRoutes/contact.route.js
+++ server/src/routes/userRoutes/contact.route.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { createContact, getAllContacts, getContactById, updateContact, deleteContact, deleteMultipleContacts, getContactStats, searchContactByEmail } from "../../controllers/usersControllers/contact.controller.js";
+import { userAuthenticated, authorizeRoles } from "../../middlewares/Auth.js";
 
 
 const router = express.Router();
 
 router.route('/').post(createContact);
-router.route('/').get(getAllContacts);
-router.route('/search').get(searchContactByEmail);
-router.route('/stats').get(getContactStats);
-router.route('/:id').get(getContactById);
-router.route('/:id').put(updateContact);
-router.route('/:id').delete(deleteContact);
-router.route('/').delete(deleteMultipleContacts);
+router.route('/').get(userAuthenticated, authorizeRoles("admin"), getAllContacts);
+router.route('/search').get(userAuthenticated, authorizeRoles("admin"), searchContactByEmail);
+router.route('/stats').get(userAuthenticated, authorizeRoles("admin"), getContactStats);
+router.route('/:id').get(userAuthenticated, authorizeRoles("admin"), getContactById);
+router.route('/:id').put(userAuthenticated, authorizeRoles("admin"), updateContact);
+router.route('/:id').delete(userAuthenticated, authorizeRoles("admin"), deleteContact);
+router.route('/').delete(userAuthenticated, authorizeRoles("admin"), deleteMultipleContacts);
 
 export default router;
