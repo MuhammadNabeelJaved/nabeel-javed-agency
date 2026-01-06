1196ff5b6a4a4e03d513787a4ef37d60544f3225
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2026-01-06 11:44:46 +0300

    Refactor project controller and routes to remove unused exports and add deleteAttachment function

--- server/src/routes/userRoutes/project.route.js
+++ server/src/routes/userRoutes/project.route.js
@@ -8,7 +8,8 @@ import {
     getProjectById,
     updateProjectStatus,
     deleteProject,
-    getProjectStats
+    getProjectStats,
+    deleteAttachment
 } from '../../controllers/usersControllers/project.controller.js';
 
 
