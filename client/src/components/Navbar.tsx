7915d252de2991bfd3aa7c18f431e3370a7a9674
MuhammadNabeelJaved <graphicsanimation786@gmail.com>
2026-06-27 19:54:07 +0500

    fix: reduce navbar logo size from h-14 to h-9

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -130,7 +130,7 @@ export function Navbar() {
               <img
                 src={logoUrl}
                 alt="CometBrew Logo"
-                className="h-14 w-auto"
+                className="h-9 w-auto"
               />
             </Link>
 
